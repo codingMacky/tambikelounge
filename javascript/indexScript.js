d15e06eac2639b2b38ca4d58ed31c24cb3b36052
markmesa-ubidy <mark.mesa@ubidy.com>
2022-09-01 08:31:25 +0800

    New design

---
+++ javascript/indexScript.js
@@ -0,0 +1,46 @@
+//Header
+function handleMobileMenuClick(type) {
+  var menuElement = document.querySelector(`${type}`);
+  menuElement.classList.toggle("show");
+}
+
+//Carousel
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function handleCarouselArrowClick(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("image-container");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+//Carousel end
+
+//redirection
+function handleTabClick(activeTab) {
+  location.href = "/product.html";
+  if (typeof Storage !== "undefined") {
+    localStorage.setItem("activeTab", activeTab);
+  }
+}
---
+++ javascript/productScript.js
@@ -0,0 +1,529 @@
+var tambikeProducts = [
+  {
+    productName: "BENKIA Black Bag",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/benkia/benkia-black-bag.png",
+    type: "gears",
+  },
+  {
+    productName: "BENKIA Leg Black Bag",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/benkia/benkia-leg-black-bag.png",
+    type: "gears",
+  },
+  {
+    productName: "BENKIA Shoes Black-Neon",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/benkia/black-neon.png",
+    type: "gears",
+  },
+  {
+    productName: "CUCYMA Tank Bag Universal",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/cucyma/cucyma-tank-bag-universal.png",
+    type: "gears",
+  },
+  {
+    productName: "Disc Plate for Click 150i",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/discPlates/click-150-i.png",
+    type: "parts",
+  },
+  {
+    productName: "Disc Plate for Mio 125",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/discPlates/mio-125.png",
+    type: "parts",
+  },
+  {
+    productName: "Disc Plate for Mio Soul i 125",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/discPlates/mio-soul-i-125.png",
+    type: "parts",
+  },
+  {
+    productName: "Disc Plate for NMAX R/F",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/discPlates/nmax-r-f.png",
+    type: "parts",
+  },
+  {
+    productName: "KMN Loud Horn and Hi-Voltage Relay",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/kmn/loud-horn-and-hi-voltage-relay.png",
+    type: "parts",
+  },
+  {
+    productName: "KOBY Disinfectant Foam",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/koby/disinfectant-foam.png",
+    type: "parts",
+  },
+  {
+    productName: "KOBY Carburator Cleaner",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/koby/carburator-cleaner.png",
+    type: "parts",
+  },
+  {
+    productName: "KOBY De-Rust Lubricating",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/koby/de-rust-lubricating.png",
+    type: "parts",
+  },
+  {
+    productName: "KOBY Tire Sealer and Inflator",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/koby/tire-sealer-and-inflator.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Headlight",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/headlight.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Mini-Driving Light LED",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/mini-driving-light-led.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Mini-Driving Light",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/mini-driving-light.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Signal Park Light Red",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/signal-park-light-red.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Signal Park Light White",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/signal-park-light-white.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Signal Park Light Yellow-Blue",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/signal-park-light-yellow-blue.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Signal Park Light Yellow",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/signal-park-light-yellow.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Muffler Cover",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/muffler-cover-1.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Muffler Cover",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/muffler-cover-2.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Muffler Cover",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/muffler-cover-3.png",
+    type: "parts",
+  },
+  {
+    productName: "LAZX Fan Cover",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/lazx/fancover.png",
+    type: "parts",
+  },
+  {
+    productName: "SKYDRIVE Switch Set",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/skydrive/master-switch-set.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Breakpad For Mio",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/break-pads.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Center Spring For Honda Click",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/center-spring-honda-click-1000-rpm.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Center Spring For Mio Sporty",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/center-spring-mio-sporty-1200-rpm.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Clutch For Beat-Fi",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/clutch-bell.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Clutch Lining 3-Pads",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/lining-3-pads.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Clutch Lining Assembly",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/lining-assembly.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Clutch Spring For Beat-Fi",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/clutch-spring-beatfi-1200-rpm.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Clutch Spring For M3",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/clutch-spring-m3-1200-rpm.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Clutch Spring For Mio 125/M3",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/clutch-spring-mio125-or-m3-1500-rpm.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Drive Face For AEROX V1/NMAX V1",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/drive-face-aeroxv1-nmax-v1.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Drive Face For AEROX V1/NMAX V1",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/drive-face-aeroxv1-nmaxv1.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Drive Face For Beat-Fi",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/drive-face-beatfi.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Fuel Injector For Click 150",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/fuel-injector-click150.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Fuel Injector For Dream-Fi",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/fuel-injector-dreamfi.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Pulley Set For Beat Carb",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/pulley-set-beatcarb.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Pulley Set For Mio 110",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/pulley-set-mio110.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Pump Filter",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/pump-filter.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Rocket Arm For Mio M3",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/rocket-arm-miom3.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Rocket Arm For NMAX",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/rocket-arm-nmax.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Rocket Arm For",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/rocket-arm1.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Rocket Arm For",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/rocket-arm2.png",
+    type: "parts",
+  },
+  {
+    productName: "YSMR Slide Piece Set For Beat-Fi",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/ysmr/slide-piece-set-beatfi.png",
+    type: "parts",
+  },
+  {
+    productName: "PLAYLIFE Handlebar Grips",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/playlife/handlebar-grips.png",
+    type: "gears",
+  },
+  {
+    productName: "ASTONE Helmet Blue-White",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/astone/helmet-blue-white.png",
+    type: "gears",
+  },
+  {
+    productName: "ASTONE Helmet Gloss-Black",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/astone/helmet-gloss-black.png",
+    type: "gears",
+  },
+  {
+    productName: "ASTONE Helmet Gloss-White",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/astone/helmet-gloss-white.png",
+    type: "gears",
+  },
+  {
+    productName: "ASTONE Helmet Gold",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/astone/helmet-gold.png",
+    type: "gears",
+  },
+  {
+    productName: "ASTONE Helmet Green",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/astone/helmet-green.png",
+    type: "gears",
+  },
+  {
+    productName: "BELL Helmet Black-Red",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/bell/helmet-black-red.png",
+    type: "gears",
+  },
+  {
+    productName: "BELL Helmet Gloss-Black",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/bell/helmet-gloss-black.png",
+    type: "gears",
+  },
+  {
+    productName: "BELL Helmet Gray-Black-White",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/bell/helmet-gray-black-white.png",
+    type: "gears",
+  },
+  {
+    productName: "BELL Helmet Green-Black",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/bell/helmet-green-black.png",
+    type: "gears",
+  },
+  {
+    productName: "BELL Helmet Matte-Black",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/bell/helmet-matte-black.png",
+    type: "gears",
+  },
+  {
+    productName: "HJC Helmet Matte-Black",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/hjc/helmet-matte-black.png",
+    type: "gears",
+  },
+  {
+    productName: "MT FALCON Helmet Black-Blue",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/mtFalcon/helmet-black-blue.png",
+    type: "gears",
+  },
+  {
+    productName: "MT FALCON Helmet Blue-Green",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/mtFalcon/helmet-blue-green.png",
+    type: "gears",
+  },
+  {
+    productName: "MT FALCON Helmet Blue-Pink",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/mtFalcon/helmet-blue-pink.png",
+    type: "gears",
+  },
+  {
+    productName: "MT FALCON Helmet Blue-Red-Violet",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/mtFalcon/helmet-blue-red-violet.png",
+    type: "gears",
+  },
+  {
+    productName: "MT FALCON Helmet White-Blue-Green",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/mtFalcon/helmet-white-blue-green.png",
+    type: "gears",
+  },
+  {
+    productName: "NHK Helmet Black-Red",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/nhk/helmet-black-red.png",
+    type: "gears",
+  },
+  {
+    productName: "NHK Helmet Blue",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/nhk/helmet-blue.png",
+    type: "gears",
+  },
+  {
+    productName: "NHK Helmet Green-Orange",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/nhk/helmet-green-orange.png",
+    type: "gears",
+  },
+  {
+    productName: "NHK Helmet Pink-Yellow",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/nhk/helmet-pink-yellow.png",
+    type: "gears",
+  },
+  {
+    productName: "DOMINO Racing Switch",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/domino/domino-racing-switch.png",
+    type: "parts",
+  },
+  {
+    productName: "FASTBIKES Shock Absorber Black-Gold",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/fastbikes/black-gold.png",
+    type: "parts",
+  },
+  {
+    productName: "FASTBIKES Shock Absorber Black-Silver",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/fastbikes/black-silver.png",
+    type: "parts",
+  },
+  {
+    productName: "FASTBIKES Shock Absorber Red-Silver",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/fastbikes/red-silver.png",
+    type: "parts",
+  },
+  {
+    productName: "FASTBIKES Shock Absorber White-Gold",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/fastbikes/white-gold.png",
+    type: "parts",
+  },
+  {
+    productName: "FASTBIKES Shock Absorber White-Silver",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/fastbikes/white-silver.png",
+    type: "parts",
+  },
+  {
+    productName: "SCOYCO MOTORCYCLE MOTORCROSS BOOTS Blue",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/scoyco/blue.png",
+    type: "gears",
+  },
+  {
+    productName: "SCOYCO MOTORCYCLE MOTORCROSS BOOTS Yellow",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/scoyco/yellow.png",
+    type: "gears",
+  },
+  {
+    productName: "SPEED MOTORCROSS BOOTS White",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/speed/high-cut-white.png",
+    type: "gears",
+  },
+  {
+    productName: "GLOBAL Signal Park Light",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/global/signal-park-light1.png",
+    type: "parts",
+  },
+  {
+    productName: "GLOBAL Signal Park Light",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/global/signal-park-light2.png",
+    type: "parts",
+  },
+  {
+    productName: "GLOBAL Signal Park Light",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/global/signal-park-light3.png",
+    type: "parts",
+  },
+  {
+    productName: "GLOBAL Signal Park Light",
+    price: "Soon",
+    imageUrl: "./tambikeProducts/global/signal-park-light4.png",
+    type: "parts",
+  },
+];
+
+//page
+var selectedItemValue = localStorage.getItem("activeTab");
+document.getElementById("activeTab").innerHTML = selectedItemValue;
+
+let selectedTab = selectedItemValue;
+var filterByActiveTab =
+  selectedTab == "Parts & Accessories" ? "parts" : "gears";
+
+document.getElementById("listOfProductContainer").innerHTML = tambikeProducts
+  .filter((tambikeProduct) => tambikeProduct.type == filterByActiveTab)
+  .map((filteredProduct) => {
+    return `<div class="product-container">
+      <div class="product-image-container">
+        <img src=${filteredProduct.imageUrl} class="product-image" />
+      </div>
+      <div class="details-container">
+        <span class="product-name">${filteredProduct.productName}</span>
+        <span class="product-price">${filteredProduct.price}</span>
+      </div>
+    </div>`;
+  })
+  .join("");
+
+//redirection
+function handleLogoClick() {
+  location.href = "/";
+}
+
+function handleTabClick(activeTab) {
+  location.href = "/product.html";
+  if (typeof Storage !== "undefined") {
+    localStorage.setItem("activeTab", activeTab);
+  }
+}
+
+//Header
+function handleMobileMenuClick(type) {
+  var menuElement = document.querySelector(`${type}`);
+  menuElement.classList.toggle("show");
+}
